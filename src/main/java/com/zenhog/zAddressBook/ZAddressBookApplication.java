edc89cf03b20745542a5e4f429b99f7d9339bfc8
Robert Fowler <robertf@ipay.co.za>
2018-05-18 16:51:42 +0200

    Project setup

---
+++ src/main/java/com/zenhog/zAddressBook/ZAddressBookApplication.java
@@ -0,0 +1,12 @@
+package com.zenhog.zAddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZAddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZAddressBookApplication.class, args);
+	}
+}
